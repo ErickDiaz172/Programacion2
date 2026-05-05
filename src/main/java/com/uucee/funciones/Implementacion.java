2a9fc798f3447e51f6d75e5aa9b6773903afe087
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2026-05-05 16:21:28 -0500

    Ahora sí, mi proyecto

---
+++ src/main/java/com/uucee/funciones/Implementacion.java
@@ -0,0 +1,29 @@
+package com.uucee.funciones;
+
+public class Implementacion{
+
+public int stop = 0; 
+public int factorial=1;
+
+public void factorial (){
+
+
+
+    for ( int i = 2 ; i <stop; i++){
+
+
+       factorial = factorial * i ;
+
+
+    }
+
+
+
+
+}
+
+
+
+
+     
+}
